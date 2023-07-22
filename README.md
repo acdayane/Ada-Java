bd130880ef5d9ffb5d92ee4ab8ddccb18ada838e
Dayane <acdayane@gmail.com>
2023-07-21 21:42:54 -0300

    complete test

---
+++ README.md
@@ -0,0 +1,43 @@
+## Getting Started
+
+Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+
+## Folder Structure
+
+The workspace contains two folders by default, where:
+
+- `src`: the folder to maintain sources
+- `lib`: the folder to maintain dependencies
+
+Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+
+> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+
+## Dependency Management
+
+The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+
+## Instructions
+
+Dada a seguinte matriz:
+
+String[][] dadosPessoas = new String[][]{ 
+    {"Ana", "Paulo", "Márcia", "Pedro",  "Beatriz"},
+    {"1.70", "1.72", "1.62", "1.90", "1.53" },
+    {"80", "90", "61", "84", "49"}
+};
+
+Faça um programa que escreva no console:
+
+    A média aritmética de altura
+    A média aritmética de peso
+    O nome e IMC de cada uma das pessoas
+    O nome da pessoa mais alta e sua altura
+    O nome da pessoa mais baixa e sua altura
+    O nome da pessoa mais pesada e seu peso
+    O nome da pessoa mais leve e seu peso
+    O nome da pessoa com o maior IMC e seu valor
+    O nome da pessoa com o menor IMC e seu valor
+
+Considere o cálculo do IMC como: IMC = (altura * altura) / peso
+
---
+++ src/App.java
@@ -0,0 +1,205 @@
+public class App {
+
+    static String[][] peopleData = new String[][]{ 
+        {"Ana", "Paulo", "Márcia", "Pedro",  "Beatriz"},
+        {"1.70", "1.72", "1.62", "1.90", "1.53" },
+        {"80", "90", "61", "84", "49"}
+    };
+
+    public static void main(String[] args) throws Exception {
+
+        getAverageHeight();
+        getAverageWeight();
+        getAllBMI();
+        getTallest();
+        getShortest();
+        getHeaviest();
+        getLightest();
+        getHighestBMI();
+        getLowestBMI();
+
+    }
+
+    private static void getAverageHeight() {
+        double sum = 0;
+
+        for(int i=0; i<peopleData[1].length; i++) {
+            sum += Double.parseDouble(peopleData[1][i]);
+        }
+
+        double average = sum/peopleData[1].length;
+
+        System.out.print("A média aritmética da altura das pessoas é ");
+        System.out.printf("%.2f", average);
+        System.out.println(" m");
+    }
+
+    private static void getAverageWeight() {
+        double sum = 0;
+
+        for(int i=0; i<peopleData[2].length; i++) {
+            sum += Double.parseDouble(peopleData[2][i]);
+        }
+
+        double average = sum/peopleData[2].length;
+
+        System.out.print("A média aritmética do peso das pessoas é ");
+        System.out.printf("%.2f", average);
+        System.out.println(" kg");
+    }
+
+    private static void getAllBMI() {
+
+        for(int i=0; i<peopleData[0].length; i++) {
+
+            String name = peopleData[0][i];
+            double height = Double.parseDouble(peopleData[1][i]);
+            double weight = Double.parseDouble(peopleData[2][i]);
+            double bmi = (height*height)/weight;
+            System.out.print("O IMC de " + name + " é ");
+            System.out.printf("%.2f", bmi);
+            System.out.println("");
+            
+        }
+    }
+
+    private static void getTallest() {
+        double tallestHeight = Double.NEGATIVE_INFINITY;
+        String name = "";
+
+        for(int i=0; i<peopleData[0].length; i++) {
+            double height = Double.parseDouble(peopleData[1][i]);
+
+            if (height > tallestHeight) {
+                tallestHeight = height;
+                name = peopleData[0][i];
+            } else {
+                if (height == tallestHeight) {
+                    name += " e " + peopleData[0][i];
+                }
+            }
+        }
+
+        System.out.print("Mais alto(s): " + name + "  mede(m) ");
+        System.out.printf("%.2f", tallestHeight);
+        System.out.println(" m");
+    }
+
+    private static void getShortest() {
+        double shortestHeight = Double.POSITIVE_INFINITY;
+        String name = "";
+
+        for(int i=0; i<peopleData[0].length; i++) {
+            double height = Double.parseDouble(peopleData[1][i]);
+
+            if (height < shortestHeight) {
+                shortestHeight = height;
+                name = peopleData[0][i];
+            } else {
+                if (height == shortestHeight) {
+                    name += " e " + peopleData[0][i];
+                }
+            }
+        }
+
+        System.out.print("Mais baixo(s): " + name + "  mede(m) ");
+        System.out.printf("%.2f", shortestHeight);
+        System.out.println(" m");
+    }
+
+
+    private static void getHeaviest() {
+        double heaviestWeight = Double.NEGATIVE_INFINITY;
+        String name = "";
+
+        for(int i=0; i<peopleData[0].length; i++) {
+            double weight = Double.parseDouble(peopleData[2][i]);
+
+            if (weight > heaviestWeight) {
+                heaviestWeight = weight;
+                name = peopleData[0][i];
+            } else {
+                if (weight == heaviestWeight) {
+                    name += " e " + peopleData[0][i];
+                }
+            }
+        }
+
+        System.out.print("Mais pesado(s): " + name + "  pesa(m) ");
+        System.out.printf("%.2f", heaviestWeight);
+        System.out.println(" kg");     
+    }
+
+    private static void getLightest() {
+        double lightestWeight = Double.POSITIVE_INFINITY;
+        String name = "";
+
+        for(int i=0; i<peopleData[0].length; i++) {
+            double weight = Double.parseDouble(peopleData[2][i]);
+
+            if (weight < lightestWeight) {
+                lightestWeight = weight;
+                name = peopleData[0][i];
+            } else {
+                if (weight == lightestWeight) {
+                    name += " e " + peopleData[0][i];
+                }
+            }
+        }
+
+        System.out.print("Mais leve(s): " + name + "  pesa(m) "); 
+        System.out.printf("%.2f", lightestWeight);
+        System.out.println(" kg");         
+    }
+
+    private static void getHighestBMI() {
+        double highestBMI = Double.NEGATIVE_INFINITY;
+        String name = "";
+        double bmi = 0.0;
+
+        for(int i=0; i<peopleData[0].length; i++) {
+            double height = Double.parseDouble(peopleData[1][i]);
+            double weight = Double.parseDouble(peopleData[2][i]);
+            bmi = (height*height)/weight;
+
+            if (bmi > highestBMI) {
+                highestBMI = bmi;
+                name = peopleData[0][i];
+            } else {
+                if (bmi == highestBMI) {
+                    name += " e " + peopleData[0][i];
+                }
+            }
+        }
+
+        System.out.print("Maior IMC: " + name + " cujo valor é ");
+        System.out.printf("%.2f", highestBMI);
+        System.out.println("");
+    }
+
+    private static void getLowestBMI() {
+        double lowestBMI = Double.POSITIVE_INFINITY;
+        String name = "";
+        double bmi = 0.0;
+
+        for(int i=0; i<peopleData[0].length; i++) {
+            double height = Double.parseDouble(peopleData[1][i]);
+            double weight = Double.parseDouble(peopleData[2][i]);
+            bmi = (height*height)/weight;
+
+            if (bmi < lowestBMI) {
+                lowestBMI = bmi;
+                name = peopleData[0][i];
+            } else {
+                if (bmi == lowestBMI) {
+                    name += " e " + peopleData[0][i];
+                }
+            }
+        }
+
+        System.out.print("Menor IMC: " + name + " cujo valor é ");
+        System.out.printf("%.2f", lowestBMI);
+        System.out.println("");
+    }
+
+}
